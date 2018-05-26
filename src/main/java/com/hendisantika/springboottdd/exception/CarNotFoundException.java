bb91512ac6040acc236f2537121f8ef71c788f62
Hendi Santika <hendisantika@gmail.com>
2018-05-27 05:22:35 +0700

    #5 - Add Car Not Found Exception

---
+++ src/main/java/com/hendisantika/springboottdd/exception/CarNotFoundException.java
@@ -0,0 +1,14 @@
+package com.hendisantika.springboottdd.exception;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-tdd
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 27/05/18
+ * Time: 05.22
+ * To change this template use File | Settings | File Templates.
+ */
+public class CarNotFoundException extends RuntimeException {
+}
